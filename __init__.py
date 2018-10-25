87264650431d03c100792c0cccc9217b25d96e51
Kris Gesling <krisgesling@gmail.com>
2018-10-26 07:58:18 +0930

    Add support for multiple movies per night; improve error handling

--- __init__.py
+++ __init__.py
@@ -2,15 +2,9 @@ from adapt.intent import IntentBuilder
 from lxml import html
 from mycroft import MycroftSkill, intent_file_handler
 from mycroft.util.log import LOG
-from mycroft.util.format import nice_time, nice_date
+from mycroft.util.format import nice_date
 from mycroft.util.parse import extract_datetime
 import requests
-# import urllib.request
-# import urllib, urllib.request
-try:
-    from mycroft.util.time import to_utc, to_local
-except Exception:
-    import pytz
 
 __author__ = 'krisgesling'
 LOGGER = LOG(__name__)
@@ -27,45 +21,61 @@ class DeckchairCinema(MycroftSkill):
             today = extract_datetime(" ")[0]
             when = extract_datetime(
                         message.data.get('utterance'), lang=self.lang)[0]
-            LOG.info("SKILL FIRED")
             # 2. Scrape website for movie on this date
             webpage = requests.get('http://www.deckchaircinema.com/program/')
             dataTree = html.fromstring(webpage.content)
             # Get list of table rows from program - these alternate between date and movie[s]
             trList = dataTree.xpath('//table[@id="program"]/tbody/tr')
-            #TODO Find child with provided date
-            movieTitle = next(
-                iter([x.getnext().getchildren()[0].getchildren()[0].text
-                for x in trList
-                if x.getchildren()[0].text==when.strftime("%A %d %B")]), "Not found"
-            )
-            #TODO test .nextchild() and return films until new date tr is found
-            #TODO if date not found test date range and return some other message
-            # 3. Construct message to return
-            self.speak_dialog('cinema.deckchair', {'when': nice_date(when, now=today), 'movieTitle': movieTitle})
-        except HTTPError as e:
-            self.__api_error(e)
+            # Find child with provided date in format "Monday 22 October"
+            dateRow = next(iter([
+                x for x in trList
+                if x.getchildren()[0].text==when.strftime("%A %-d %B")
+            ]), None)
+            # Test next row and add films to list until non-film row is found
+            moviesOnDate = []
+            def addMovieFromDate(row):
+                if row.getnext().getchildren()[0].get("class") == "program-film":
+                    moviesOnDate.append(row.getnext())
+                    return addMovieFromDate(row.getnext())
+                else:
+                    return None
+            addMovieFromDate(dateRow)
+            # 3. If date not found test date range and return some other message
+            # 4. Construct message to return
+            movieDetails = ""
+            for movie in moviesOnDate:
+                if len(movieDetails)>0:
+                    movieDetails+=", and "
+                # Movie title
+                movieDetails+=movie.getchildren()[0].getchildren()[0].text
+                movieDetails+=" at "
+                # Movie time
+                movieDetails+=movie.getchildren()[1].text
+                ### OTHER DETAILS AVAILABLE ON SCRAPED PAGE
+                # Length of film = movie.getchildren()[2].text
+                # Film age rating = movie.getchildren()[3].text
+                # Film location [deckchair or other cinema] = movie.getchildren()[4].text
+                # Additional options in [5].getchildren() are:
+                    # [film details via separate page,
+                    #  youtube trailer,
+                    #  buy tickets,
+                    #  ical calendar item]
+
+            self.speak_dialog('cinema.deckchair', {'when': nice_date(when, now=today), 'movieDetails': movieDetails})
+        # TODO look at error handling of requests module, this is taken from urllib
+        except ( requests.exceptions.ConnectionError
+            or requests.exceptions.HTTPError
+            or requests.exceptions.Timeout
+            or requests.exceptions.TooManyRedirects
+            ) as e:
+            LOG.error("Error: {0}".format(e))
+            self.speak_dialog('error.http')
         except Exception as e:
             LOG.error("Error: {0}".format(e))
             self.speak_dialog('error')
 
     def stop(self):
         pass
-
-    def __api_error(self, e):
-        if e.response.status_code == 401:
-            from mycroft import Message
-            self.bus.emit(Message("mycroft.not.paired"))
-
-    def __to_UTC(self, when):
-        try:
-            # First try with modern mycroft.util.time functions
-            return to_utc(when)
-        except Exception:
-            # TODO: This uses the device timezone -- should probably use
-            #       the timezone of the location being forecasted
-            timezone = pytz.timezone(self.location["timezone"]["code"])
-            return timezone.localize(when).astimezone(pytz.utc)
 
 def create_skill():
     return DeckchairCinema()
